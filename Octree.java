a651986fb7c2bcfd906346da124085d3a828fcda
LeoGrin <leo.grinsztajn@polytechnique.edu>
2019-01-22 20:51:14 +0100

    octree computation

--- Octree.java
+++ Octree.java
@@ -1,6 +1,11 @@
 
 import Jcg.geometry.Point_3;
 import Jcg.geometry.Vector_3;
+import Jcg.geometry.PointCloud_3;
+
+import java.awt.*;
+import java.util.ArrayList;
+import java.util.Arrays;
 
 /**
  * A class for representing an Octree
@@ -12,7 +17,28 @@ public class Octree {
 	public OctreeNode root;
 	
 	public Octree(Point_3[] points){
-		throw new Error("To be completed");
-	}
+        /**
+         * Compute the bounding box
+         */
+        ArrayList<Point_3> points_list = new ArrayList<Point_3>();
+        PointCloud_3 pointCloud = new PointCloud_3(points_list);
+        Point_3[] cube = pointCloud.boundingBox();
+
+        /**
+         * Compute the center and the side length of the bounding box
+         */
+        Number coefficient[] = new Number[2]; // we want the mean of the cube, so we take the mean of the bounding box. But which form has the bounding box ???
+        Arrays.fill(coefficient, 1 / 2);
+        Point_3 center = Point_3.linearCombination(cube, coefficient);
+        float a = (float)pointCloud.min(0).distanceFrom(pointCloud.max(0)); //length of the side of the cube
+
+        /**
+         * Initialize the root node
+         */
+        root = new OctreeNode(Arrays.asList(points), center, null, a, 0);
+
+
+
+    }
 	
 }
--- OctreeNode.java
+++ OctreeNode.java
@@ -1,5 +1,10 @@
+import java.awt.*;
+import java.lang.reflect.Array;
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
+import Jcg.geometry.PointCloud_3;
 import Jcg.geometry.Point_3;
 
 /**
@@ -12,20 +17,80 @@ public class OctreeNode {
 	public int level;
 	public OctreeNode[] children=null;
 	public OctreeNode father;
-	public Point_3 p; //point stored in a leaf
+	public Point_3 p; //point stored in a leaf or the middle of the box if the node isn't a leaf
+	public float a; // length of the side of the cube
 	
 	/**
 	 * Create the octree for storing an input point cloud
 	 */
-	public OctreeNode(List<Point_3> points) {
-		throw new Error("To be completed");
+	public OctreeNode(List<Point_3> points, Point_3 p, OctreeNode father, float a, int level) {
+		int i;
+		Point_3 new_center;
+		this.father = father;
+		this.p = p;
+		this.a = a;
+		this.level = level;
+
+
+		/**
+		 * If the node is a leaf, set the point stored
+		 */
+		if (points.size() <= 1){
+			if (points.size() == 0)
+				this.p = null;
+			else
+				this.p = points.get(0);
+		}
+		else{
+			/**
+			 * Compute the points going in each new cube
+			 */
+			children = new OctreeNode[8];
+			ArrayList<Point_3>[] children_points = new ArrayList[8];
+
+			i = 0;
+			for (int add_0=-1; add_0 <= 1; add_0 = add_0 + 2) {
+				for (int add_1 = -1; add_1 <= 1; add_1 = add_1 + 2) {
+					for (int add_2 = -1; add_2 <= 1; add_2 = add_2 + 2) {
+
+						for (Point_3 point : points) {
+							if ((point.x - p.x) * add_0 >= 0 && (point.y - p.y) * add_1 >=0 &&(point.z - p.z) * add_2 >=0)
+								children_points[i].add(point);
+						}
+						i++;
+					}
+
+				}
+			}
+
+			/**
+			 * Initialize each children
+			 */
+			i = 0;
+			for (int add_0=-1; add_0 <= 1; add_0 = add_0 + 2){
+				for (int add_1=-1; add_1 <= 1; add_1 = add_1 + 2){
+					for (int add_2=-1; add_2 <= 1; add_2 = add_2 + 2){
+						new_center = new Point_3(p.x + add_0 * (a / 4), p.y + add_1 * (a / 4), p.z + add_2 * (a / 4)); //compute the center of the new node
+						children[i] = new OctreeNode(children_points[i], new_center , this, a / 2, level + 1);
+						i++;
+					}
+				}
+
+			}
+
+
+		}
+
+
+
+
 	}
 	
 	/**
 	 * Add a node into the OctreeNode
 	 */
 	public void add(Point_3 p) {
-		throw new Error("To be completed");
+		;
 	}
 		
 }
