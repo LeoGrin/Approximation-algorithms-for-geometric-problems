3077fb9c58389b938217130da9c5a1a606a8d1f8
compteexo <46935253+compteexo@users.noreply.github.com>
2019-01-30 09:50:02 +0100

    Update Octree.java

--- Octree.java
+++ Octree.java
@@ -1,6 +1,4 @@
 
-
-
 import Jcg.geometry.Point_3;
 import Jcg.geometry.Vector_3;
 import Jcg.geometry.PointCloud_3;
@@ -11,19 +9,22 @@ import java.util.Arrays;
 
 /**
  * A class for representing an Octree
- * 
+ *
  * @author Luca Castelli Aleardi, Ecole Polytechnique
  * @version december 2018
  */
 public class Octree {
-	public OctreeNode root;
-	
-	public Octree(Point_3[] points){
+
+    public OctreeNode root;
+
+    public Octree(Point_3[] points) {
         /**
          * Compute the bounding box
          */
         ArrayList<Point_3> points_list = new ArrayList<>(points.length);
-        for(Point_3 p : points){points_list.add(p);}
+        for (Point_3 p : points) {
+            points_list.add(p);
+        }
         PointCloud_3 pointCloud = new PointCloud_3(points_list);
         Point_3[] cube = pointCloud.boundingBox();
 
@@ -33,15 +34,33 @@ public class Octree {
         Number coefficient[] = new Number[2]; // we want the mean of the cube, so we take the mean of the bounding box. But which form has the bounding box ???
         Arrays.fill(coefficient, 0.5);
         Point_3 center = Point_3.linearCombination(cube, coefficient);
-        double a = (double)pointCloud.min(0).distanceFrom(pointCloud.max(0)); //length of the side of the cube
+        double a = Math.max(Math.max(cube[1].x - cube[0].x, cube[1].y - cube[0].y), cube[1].z - cube[0].z) * 1.000000001;
 
+        //(double)pointCloud.min(0).distanceFrom(pointCloud.max(0)); //length of the side of the cube
         /**
          * Initialize the root node
          */
-        root = new OctreeNode(Arrays.asList(points), center, null, a, 0);
-
-
+        root = new OctreeNode(points_list, center, null, a, 0,0);
 
     }
-	
+
+   static double calc_a(ArrayList<Point_3> points_list) {
+       if(points_list.isEmpty()) return 0;
+        PointCloud_3 pointCloud = new PointCloud_3(points_list);
+        Point_3[] cube = pointCloud.boundingBox();
+
+        /**
+         * Compute the center and the side length of the bounding box
+         */
+        return Math.max(Math.max(cube[1].x - cube[0].x, cube[1].y - cube[0].y), cube[1].z - cube[0].z) * 1.000000001;
+    }
+
+  static  Point_3 calc_p(ArrayList<Point_3> points_list) {
+      if(points_list.isEmpty()) return null;
+        PointCloud_3 pointCloud = new PointCloud_3(points_list);
+        Point_3[] cube = pointCloud.boundingBox();
+        Number coefficient[] = new Number[2]; // we want the mean of the cube, so we take the mean of the bounding box. But which form has the bounding box ???
+        Arrays.fill(coefficient, 0.5);
+        return Point_3.linearCombination(cube, coefficient);
+    }
 }
